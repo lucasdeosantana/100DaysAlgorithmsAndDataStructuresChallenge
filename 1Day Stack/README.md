98d9b3c98bbabdda6ead1d2e94020a8ba44be32b
Lucas Santana <lucasdeosantana@gmail.com>
2021-01-19 15:59:21 -0300

    Add Code Day 1, start add MarkDown

---
+++ 1Day Stack/README.md
@@ -0,0 +1,4 @@
+# 100DaysAlgorithmsAndDataStructuresChallenge
+## Day One A Stack!
+
+A Stack is an abstract data type that serves as a collection of elements. The Stack use a order of elements named LIFO(Last In, First Out), this data type is useful when is necessary save commands to 
---
+++ 1Day Stack/Stack.js
@@ -0,0 +1,37 @@
+class Stack{
+    _stackMemory = []
+    _top = 0
+    constructor(size) {
+        this.size = size - 1 
+    }
+
+    push(value) {
+        if(this._top<this.size){
+            this._stackMemory.push(value)
+            this._top+=1
+            return this.top-1
+        }else{
+            return null
+        }
+    }
+    pop(value){
+        if(this._top>0){
+            this._top-=1
+            return this._stackMemory.pop()
+        }else{
+            return null
+        }
+    }
+    top(){
+        if(this._top>0){
+            return this._stackMemory[this._top-1]
+        }else{
+            return null
+        }
+    }
+    copy(){
+        const copyThis = new this.constructor(this.size+1)
+        this._stackMemory.map(value=>copyThis.push(value))
+        return copyThis
+    }
+}
